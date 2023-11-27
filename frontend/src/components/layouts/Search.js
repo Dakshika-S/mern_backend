7b1c1b6fc0489103cb81a2cbfda74731938ae64b
dakshika <dakshikarajah95@gmail.com>
2023-11-27 23:51:16 +0530

    search comp

--- frontend/src/components/layouts/Search.js
+++ frontend/src/components/layouts/Search.js
@@ -1,6 +1,13 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function Search() {
+  const navigate = useNavigate();
+  const [keyword, setkeyword] = useState();
+
   const searchHandler = (e) => {
     e.preventDefault(); //wiill disable auto reloading/loading when submitting
+    navigate("/search/");
   };
   return (
     <div className="input-group">

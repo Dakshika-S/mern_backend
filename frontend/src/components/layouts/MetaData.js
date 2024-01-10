f6e2900e734f6c1b0085b9c9f29744768e281093
dakshika <dakshikarajah95@gmail.com>
2024-01-10 12:23:08 +0530

    created sep repo for backend

--- frontend/src/components/layouts/MetaData.js
+++
@@ -1,9 +0,0 @@
-import { Helmet } from "react-helmet-async"; //this wil be dislplayed in title of browser tab
-
-export default function MetaData({ title }) {
-  return (
-    <Helmet>
-      <title>{`${title} - JVLcart`}</title>
-    </Helmet>
-  );
-}
